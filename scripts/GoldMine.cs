144f5c45ee66459db06f04576d4740dce477196d
Emrah Atalay <emrahatalay92@gmail.com>
2024-11-23 20:35:22 +0300

    first commit

---
+++ scripts/GoldMine.cs
@@ -0,0 +1,10 @@
+using Godot;
+
+namespace TinySwords.scripts;
+
+public partial class GoldMine : Sprite2D
+{
+    [Export] private Texture2D _goldMineInActive;
+    [Export] private Texture2D _goldMineActive;
+    
+}
